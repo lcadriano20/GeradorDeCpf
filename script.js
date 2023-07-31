f2b9d787495cd273ad596def384dfde210568df0
AdrianoC <adriano.cbagc@gmail.com>
2023-07-31 10:00:50 -0300

    arquivos atualizados

---
+++ script.js
@@ -0,0 +1,40 @@
+// CAPTURAR ELEMENTOS 
+
+const cpfEl          = document.querySelector("#cpf")
+const gerarCpfBtn    = document.querySelector("#gerar-cpf")
+const copiarCpfBtn   = document.querySelector("#copiar-cpf")
+const copiadoSucesso = document.querySelector("#copiado-sucesso")
+
+// Função que vai gerar o CPF 
+function gerarCPF() {
+    let n = Math.floor(Math.random()*999999999)+1
+    let nStr = n.toString().padStart(9,"0")
+    let dv1 = calcularDV(nStr,10);
+    let dv2 = calcularDV(nStr+dv1,11);
+
+    cpfEl.innerText = formatarCPF(nStr+dv1+dv2);
+    
+}
+function calcularDV(numero,peso) {
+    let total = 0; 
+    for (let i=0;i<numero.length;i++) {
+        total += parseInt(numero.charAt(i)*peso--)
+    }
+    let resto = total % 11;
+    return resto < 2 ? 0:11 - resto;
+}
+function formatarCPF(cpf) {
+    const regex = /^(\d{3})(\d{3})(\d{3})(\d{2})$/;
+    return cpf.replace(regex,"$1.$2.$3-$4")
+}
+function copiarCPF() {
+ const cpf = cpfEl.innerText
+ navigator.clipboard.writeText(cpf).then(()=> {
+    copiadoSucesso.innerText = "Copiado com sucesso!"
+ },(err)=>{     
+    copiadoSucesso.classList.add('error-message')
+ })
+}
+
+gerarCpfBtn.addEventListener("click",gerarCPF)
+copiarCpfBtn.addEventListener("click",copiarCPF)
